c90d843efbc893b8e589cd355e0362f371d09c28
encadyma <encadyma@gmail.com>
2017-06-12 22:33:21 -0700

    Added includes/admin.php include file template for admin panel functionality

---
+++ includes/admin.php
@@ -0,0 +1,11 @@
+<?php
+  function deny_announcement() {
+    // code to deny announcement here...
+  }
+  function allow_announcemnt() {
+    // code to allow announcement here...
+  }
+  function edit_announcement() {
+    // code to edit announcement here...
+  }
+?>
