eda7fa494dcaad8f223dcfd6ffa496ef8b83a821
encadyma <encadyma@gmail.com>
2017-06-13 23:13:58 -0700

    Added 404.php for handling pages not found

---
+++ 404.php
@@ -0,0 +1,34 @@
+<html>
+  <head>
+    <title>Spartan Connect: 404 Not Found</title>
+
+    <meta charset=UTF-8>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Welcome to Spartan Connect, La Cañada High School\'s official student connection program.">
+
+    <meta property="og:title" content="404 Not Found">
+    <meta property="og:description" content="Welcome to Spartan Connect, La Cañada High School\'s official student connection program.">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Spartan Connect">
+    <meta property="og:url" content="http://www.connect.lchsspartans.net/">
+    <meta property="og:image" content="">
+
+    <style>
+      @import url('https://fonts.googleapis.com/css?family=Cookie|Libre+Baskerville|Overpass');
+      body {
+        font-family: Overpass, sans-serif;
+        margin: 0;
+        padding: 0;
+      }
+      h1 {
+        font-size: 4em;
+      }
+    </style>
+  </head>
+  <body>
+    <div style="text-align: center; margin: 20vh auto;">
+      <h1>404</h1>
+      <h3>The page you were looking for could not be found.</h3>
+    </div>
+  </body>
+</html>
