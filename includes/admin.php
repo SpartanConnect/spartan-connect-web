45eeec9ae38ed9e9739209a047a6585ae10ce1ed
encadyma <encadyma@gmail.com>
2017-06-15 16:02:24 -0700

    Reformatted email string

--- includes/admin.php
+++ includes/admin.php
@@ -1,14 +1,39 @@
 <?php
-  function deny_announcement($reason, $name, $description, $email) {
-    $subject = "Rejected Announcement"
-    $body = "Hello ".$name.", <br><br>"."We regret to inform you that your announcement '".$name."' request was rejected by an administrator.\n\r\n\rDescription:\n\r".$description."\n\r\n\rThe reason for which your announcement was denied, as described by the administrator are included below:\n\r\n\r".$reason;
+  include('secret.php');
+
+  function deny_announcement($name, $email, $reason, $announcement_title, $announcement_description) {
+    $link = LOCAL_URL."user_panel.php";
+    $subject = "Rejected Announcement";
+    $body = <<<EOF
+
+===========================
+
+Hello {$name},
+
+We regret to inform you that your announcement '{$announcement_title}' was rejected by an administrator.
+
+Title: {$announcement_title}
+Description:
+{$announcement_description}
+
+Reason for Denial:
+{$reason}
+
+You may edit and resubmit your announcement by going to {$link}.
+
+- Spartan Connect Student Development Team
+
+===========================
+
+Do not reply to this email.
+EOF;
     $headers = 'To: '.$name.' '.$email."\r\n".
         'From: studentdevteam@lcusd.net'."\r\n".
         'Reply-To: studentdevteam@lcusd.net'."\r\n".
         'X-Mailer: PHP/'.phpversion();
     mail($email, $subject, $body, $headers);
   }
-  function allow_announcemnt() {
+  function allow_announcement() {
     // code to allow announcement here...
   }
   function edit_announcement() {
