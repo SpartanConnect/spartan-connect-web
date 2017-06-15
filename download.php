25715501a65b48e85bda9d1a6a78bd07f31882cf
KJ John <kamyarusa@yahoo.com>
2017-06-14 20:20:18 -0700

    Fixed Formatting

--- download.php
+++ download.php
@@ -1,7 +1,7 @@
 <?php
   include('includes/include.php');
   include('includes/announcements.php');
-  header('Content-Type: text/csv; charset=utf-8');
+  header('Content-Type: text/csv; charset=utf-16');
   header('Content-Disposition: attachment; filename=announcements.csv');
   $output = fopen('php://output', 'w');
   fputcsv($output, array('Name', 'Description', 'Start Date', 'End Date', 'Teacher'));
@@ -9,8 +9,8 @@
 
   foreach ($rows as $row) {
     $result = array(
-      'name' => mb_convert_encoding($row['name'], "UTF-16"),
-      'description' => mb_convert_encoding($row['description'], "UTF-16"),
+      'name' => mb_convert_encoding($row['name'], "UTF-8", "HTML-ENTITIES"),
+      'description' => mb_convert_encoding($row['description'], "UTF-8", "HTML-ENTITIES"),
       'startDate' => $row['startDate'],
       'endDate' => $row['endDate'],
       'teacher' => get_teacher($row['teacherID'])
