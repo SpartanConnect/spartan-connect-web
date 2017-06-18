9f8a836b3223933df824aef9685e49133f49217d
KJ John <kamyarusa@yahoo.com>
2017-06-17 19:12:55 -0700

    Merged Admin and Announcements

--- includes/announcements.php
+++ includes/announcements.php
@@ -31,8 +31,45 @@
     ))[0];
   }
 
-  function deny_announcement($id) {
-    return perform_query("UPDATE ".DB_TABLE_ANNOUNCEMENTS." SET `approved`=2 WHERE `id` = :id", array(
+  function deny_announcement($id, $reason) {
+    $announcement = get_announcement_by_id($id);
+    $teacher = get_all_teacher($id);
+    $name = $teacher['name'];
+    $email = $teacher['email'];
+    $announcement_title = $announcement['name'];
+    $announcement_description = $announcement['description'];
+
+    $link = LOCAL_URL."user_panel.php";
+    $subject = "Rejected Announcement '".$announcement_title."'";
+    $body = <<<EOF
+
+===========================
+
+Hello {$name},
+
+We regret to inform you that your announcement '{$announcement_title}' was rejected by an administrator.
+
+Title: {$announcement_title}
+Description:
+{$announcement_description}
+
+Reason for Denial:
+{$reason}
+
+You may edit and resubmit your announcement by going to {$link}.
+
+- Spartan Connect Student Development Team
+
+===========================
+
+Do not reply to this email.
+EOF;
+    $headers = 'To: '.$name.' '.$email."\r\n".
+        'From: studentdevteam@lcusd.net'."\r\n".
+        'Reply-To: studentdevteam@lcusd.net'."\r\n".
+        'X-Mailer: PHP/'.phpversion();
+    mail($email, $subject, $body, $headers);
+    perform_query("UPDATE ".DB_TABLE_ANNOUNCEMENTS." SET `approved`=2 WHERE `id` = :id", array(
       ':id' => $id
     ));
   }
